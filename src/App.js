99891c4a31646d2eb446d504759d000108a72301
Lucas Santos do Amaral <lucas.amaral@MJV.net>
2019-02-26 14:11:40 -0300

    Ajustes nos componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Title from './Title';
 import Lista from './Lista';
+import './App.scss';
+
+const b = ['Huguinhho', 'Zezinho', 'Luisinho'];
 
 const App = (props) => {
     return (
         
-        <div>
-            <Title title="Título"></Title>
+        <div className="container">
+            <Title title="Lista Componentizada"></Title>
             <Lista/>
+            <Title title="Lista vinda de um array"></Title>
+            <ul>
+                {b.map(i=>{
+                    return <li>{i}</li>
+                })}
+            </ul>
         </div>
         
     )
--- src/Title.jsx
+++ src/Title.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Title extends Component {
     render(){
         return(
-            <div><h1>{this.props.title}</h1></div>
+            <h1>{this.props.title}</h1>
         )
     }
 }
