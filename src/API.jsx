cfcdfe0e9b2ee7589a563c9fd637ae71869f3cb9
Lucas Santos do Amaral <lucas.amaral@MJV.net>
2019-03-19 14:52:19 -0300

    Atribuindo quantidade de array ao estado

--- src/API.jsx
+++ src/API.jsx
@@ -3,36 +3,38 @@ import axios from 'axios';
 
 class API extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            planets: []
+            planets: [],
+            qtdFilms: 0
         }
     }
 
-    componentDidMount(){
-        axios.get('https://swapi.co/api/planets/3/')
-        .then(response => {
-            this.setState({planets: response.data})
-        })
-        .catch(function(error){
-            console.log(error);
-        })
+    componentDidMount() {
+        axios.get('https://swapi.co/api/planets/1/')
+            .then(response => {
+                this.setState({ planets: response.data, qtdFilms: response.data.films.length })
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 
-    renderPlanets(){
-            return(
-                <div>
-                    <p>Nome: {this.state.planets.name}</p>
-                    <p>Clima: {this.state.planets.climate}</p>
-                    <p>Terreno: {this.state.planets.terrain}</p>
-                    <p>População: {this.state.planets.population}</p>
-                </div>
-            )
-        
+    renderPlanets() {
+        return (
+            <div>
+                <p>Nome: {this.state.planets.name}</p>
+                <p>Clima: {this.state.planets.climate}</p>
+                <p>Terreno: {this.state.planets.terrain}</p>
+                <p>População: {this.state.planets.population}</p>
+                <p>{this.state.qtdFilms === 0 ? 'Este planeta não apareceu em nenhum filme' : this.state.qtdFilms === 1 ? `Este planeta aparece em ${this.state.qtdFilms} filme` : this.state.qtdFilms > 1 ? `Este planeta aparece em ${this.state.qtdFilms} filmes` : ''}</p>
+            </div>
+        )
+
     }
 
-    render(){
+    render() {
         return this.renderPlanets()
     }
 }
